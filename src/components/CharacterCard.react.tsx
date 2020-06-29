e7743a3999b69bfe78b139396f1422aea0081b2f
Jacob Rienstra <jacobdrienstra@gmail.com>
2020-06-29 02:52:09 -0400

    stylelint and flipipng

--- src/components/CharacterCard.react.tsx
+++ src/components/CharacterCard.react.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { cx } from "emotion";
 import { css } from "@emotion/core";
 
 type Props = {
@@ -22,31 +23,77 @@ function getFileName(f: string): string {
 }
 
 const card = css`
-  padding: 8px;
-  background: #e0e0e0;
-  border: 2px solid black;
-  border-radius: 6px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
-  cursor: pointer;
+  position: relative;
+  display: block;
   flex: 0 1 auto;
+  box-sizing: border-box; /* width: 100px; */
   margin: 16px;
-  width: 100px;
-  box-sizing: content-box;
 
-  & .name {
+  /* entire container, keeps perspective */
+  perspective: 1000px;
+  cursor: pointer;
+
+  .name {
+    padding-top: 4px;
     color: black;
     font-weight: 700;
     font-size: 18px;
-    word-wrap: break-word;
     text-align: center;
-    padding-top: 4px;
+    word-wrap: normal;
   }
 
-  & .back {
+  /* flip speed goes here */
+  .flipper {
+    position: relative;
+    transform-style: preserve-3d;
+    transition: 0.3s;
+  }
+
+  /* flip the pane when hovered */
+  &.eliminated .flipper {
+    transform: rotateY(180deg);
+  }
+
+  &,
+  .front,
+  .back {
+    width: 116px;
+    height: 180px;
+  }
+
+  /* hide back of pane during swap */
+  .front,
+  .back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    border: 2px solid black;
+    border-radius: 6px;
+    backface-visibility: hidden;
+  }
+
+  /* front pane, placed above back */
+  .front {
+    z-index: 2;
+    background: #e0e0e0;
+
+    /* for firefox 31 */
+    transform: rotateY(0deg);
+    .container {
+      padding: 8px;
+    }
+  }
+
+  /* back, initially hidden pane */
+  .back {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     background: #00a0ff;
+    transform: rotateY(180deg);
+    .name {
+      color: #e0e0e0;
+    }
   }
 `;
 
@@ -55,15 +102,22 @@ function CharacterCard(props: Props): JSX.Element {
   const [isVisible, setVisible] = React.useState(true);
   const name = capitalize(getFileName(fileSrc));
   return (
-    <div css={card} onClick={(): void => setVisible(!isVisible)}>
-      {isVisible ? (
-        <>
-          <img src={fileSrc} alt={name} width={100} />
-          <div className="name">{name}</div>{" "}
-        </>
-      ) : (
-        <div className="back" />
-      )}
+    <div
+      css={card}
+      className={cx({ eliminated: !isVisible }, ["card", "flip-container"])}
+      onClick={(): void => setVisible(!isVisible)}
+    >
+      <div className="flipper">
+        <div className="front">
+          <div className="container">
+            <img src={fileSrc} alt={name} width={100} />
+            <div className="name">{name}</div>
+          </div>
+        </div>
+        <div className="back">
+          <div className="name">{name}</div>
+        </div>
+      </div>
     </div>
   );
 }
