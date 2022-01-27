e272c591ed6147e0ffb1f8cda5d277388924381a
Jacob Rienstra <jacobdrienstra@gmail.com>
2022-01-27 00:14:54 -0500

    Updates

---
+++ precache-manifest.567a8b0c8377d61b6294c18dca97a549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f574baf87ea0afe2d2765b2de4c474",
+    "url": "/guesswho/index.html"
+  },
+  {
+    "revision": "5c11ebc5586138195e9c",
+    "url": "/guesswho/static/css/main.f9aaf92d.chunk.css"
+  },
+  {
+    "revision": "a9dc19624d6454f66597",
+    "url": "/guesswho/static/js/2.f5e49f71.chunk.js"
+  },
+  {
+    "revision": "7f1ab47ca337195e40670f43d20b8f36",
+    "url": "/guesswho/static/js/2.f5e49f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c11ebc5586138195e9c",
+    "url": "/guesswho/static/js/main.a7cfd463.chunk.js"
+  },
+  {
+    "revision": "9a2681c01a9451179e58",
+    "url": "/guesswho/static/js/runtime-main.378ecd15.js"
+  }
+]);
